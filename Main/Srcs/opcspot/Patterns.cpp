ba898e85cf1928dd7fb5b3362f2b4d1c3d2b187b
huayunfly <huayunfly@gmail.com>
2010-02-23 09:24:10 +0000

    add new methods for zsdriver remove the connect parameters from zsdriver.xml add new files: Patterns.h .cpp

---
+++ Main/Srcs/opcspot/Patterns.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "Patterns.h"
+
+std::string XmlDocument::xmlFile;
--- Main/Srcs/opcspot/ZSSerial.h
+++ Main/Srcs/opcspot/ZSSerial.h
@@ -11,24 +11,67 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
 #define BOOST_SYSTEM_DYN_LINK
 #define BOOST_DATE_TIME_DYN_LINK
-//#define BOOST_REGEX_DYN_LINK	// for "boost/asio.hpp"
 #include "boost/asio/serial_port.hpp"
+#include "boost/variant.hpp"
 
 class ZSSerial
 {
 public:
+	enum DataGroup { one, two };
+	typedef boost::variant<int, float> ZSVariant;
+
+	// Constructor.
+	// @param <port> device name, example "/dev/ttyUSB0" or "COM4"
+	// @param <baudRate> communication speed, example 9600 or 115200
+	// @throws boost::system::system_error if cannot open the serial device
 	ZSSerial(std::string port, unsigned int baudRate);
 	~ZSSerial(void);
 
 public:
+	// Whether the serial port is opened.
+	// @return true if the serial is opened, otherwise false
 	bool IsOpened()
 	{
 		return serial.is_open();
 	}
 
+	// Read data from the device.
+	// @param <group> one represents the frequent access data set,
+	//				two represents the less frequent access data set
+	// @return a vector containing the safe and generic union
+	// @throws boost::system::system_error on failure
+	std::vector<ZSVariant> ReadData(DataGroup group);
+
+	// Write data to the device
+	// @param <dataID> the data ID
+	// @param <data> the data content represented by a safe union
+	// @throws boost::system::system_error on failure
+	void WriteData(int dataID, const ZSVariant& data);
+
+	// Write command to the device
+	// @param <commandID> command ID
+	// @throws boost::system::system_error on failure
+	void WriteCommand(int commandID);
+
+	// Calculate the sum of BCD codes
+	// @param <bcdVec> the given BCD vector
+	// @return the sum
+	static unsigned char CalculateBCDSum(const std::vector<unsigned char>& bcdVec);
+
+private:
+	// Check BCD code sum
+	// @param <bcdVec> the given BCD vector
+	// @param <expected> the expected value
+	// @return true if the BCD sum is equal to expected value, otherwise false
+	bool CheckSumEqual(const std::vector<unsigned char>& bcdVec, unsigned char expected);
+
 private:
 	boost::asio::io_service io;
 	boost::asio::serial_port serial;
+	static const unsigned char begin;
+	static const unsigned char end;
 };
