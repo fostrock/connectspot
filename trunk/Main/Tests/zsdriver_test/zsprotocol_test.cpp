9f18d91ab1dcd68fd5f379ed390a64a3c76a0257
huayunfly@gmail.com <huayunfly@gmail.com@c85c812c-0970-11df-bd4f-1fc3cb39d6f8>
2010-02-24 08:04:44 +0000

    update test case

--- trunk/Main/Tests/zsdriver_test/zsprotocol_test.cpp
+++ trunk/Main/Tests/zsdriver_test/zsprotocol_test.cpp
@@ -28,16 +28,34 @@ BOOST_AUTO_TEST_CASE(zsprotocol_parse_test)
 	ZSSerialProtocol::DataSetDef::const_iterator it = dataset.find(1);
 	BOOST_CHECK(it != dataset.end());
 
+	// test read data cmd part
 	const std::vector<ZSReadDataCmd>& readDataCmd = protocol.GetReadDataCmd();
 	BOOST_CHECK(2 == readDataCmd.size());
 	ZSReadDataCmd::OffsetDef::const_iterator itRead = readDataCmd.at(1).offset.find(21);
 	BOOST_CHECK(itRead != readDataCmd.at(1).offset.end());
+	BOOST_CHECK_EQUAL(itRead->second, 0);
 
+	itRead = readDataCmd.at(1).offset.find(42);
+	BOOST_CHECK(itRead != readDataCmd.at(1).offset.end());
+	BOOST_CHECK_EQUAL(itRead->second, 62);
+
+	// test write data cmd part
 	const ZSWriteDataCmd& writeDataCmd = protocol.GetWriteDataCmd();
 	ZSWriteDataCmd::ParamDef::const_iterator itWrite = writeDataCmd.param.find(1);
 	BOOST_CHECK(itWrite != writeDataCmd.param.end());
+	BOOST_CHECK_EQUAL(itWrite->second, 1);
 
+	itWrite = writeDataCmd.param.find(42);
+	BOOST_CHECK(itWrite != writeDataCmd.param.end());
+	BOOST_CHECK_EQUAL(itWrite->second, 23);
+
+	// test common cmd part
 	const ZSSerialProtocol::CommonCmdDef& commonCmd = protocol.GetCommonCmd();
 	ZSSerialProtocol::CommonCmdDef::const_iterator itCmd = commonCmd.find(61);
 	BOOST_CHECK(itCmd != commonCmd.end());
+	BOOST_CHECK_EQUAL(itCmd->second, 0x10);
+
+	itCmd = commonCmd.find(64);
+	BOOST_CHECK(itCmd != commonCmd.end());
+	BOOST_CHECK_EQUAL(itCmd->second, 0x13);
 }
