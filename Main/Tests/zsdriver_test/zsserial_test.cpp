0bf89bbbc97d63513697be80a2de9d6941d33ae6
huayunfly <huayunfly@gmail.com>
2010-03-03 09:14:37 +0000

    add a test fixture dealing with ZSSerialProtocol

--- Main/Tests/zsdriver_test/zsserial_test.cpp
+++ Main/Tests/zsdriver_test/zsserial_test.cpp
@@ -14,9 +14,30 @@
 
 const std::string zs_protocol_file = "E:\\My Projects\\connectspot\\Main\\bin\\config\\zsdriver.xml";
 
+class TestFixture {
+public:
+	TestFixture() : protocol(zs_protocol_file) 
+	{ 
+		bool isOK = protocol.Parse();
+		if (!isOK)
+		{
+			throw new std::logic_error("parse protocol file failed");
+		}
+		BOOST_TEST_MESSAGE("setup fixture");
+	}
+	~TestFixture()
+	{ 
+		BOOST_TEST_MESSAGE("teardown fixture");
+	}
+
+public:
+	ZSSerialProtocol protocol;
+};
+
+BOOST_FIXTURE_TEST_SUITE(zsserial_suite, TestFixture)
+
 BOOST_AUTO_TEST_CASE(zsserial_constructors_test)
 {
-	ZSSerialProtocol protocol(zs_protocol_file);
 	ZSSerial zs("COM3", 9600, protocol);
 	BOOST_CHECK(zs.IsOpened());
 }
@@ -42,10 +63,25 @@ BOOST_AUTO_TEST_CASE(zsserial_Dec2BCD_test)
 	BOOST_CHECK(0x09 == ZSSerial::Dec2BCD(9));
 }
 
+BOOST_AUTO_TEST_CASE(zsserial_MakeReadCmd_test)
+{
+	ZSSerial zs("COM3", 9600, protocol);
+	std::string readCmd;
+	
+	readCmd += 0xbb;
+	readCmd += 0x21;
+	readCmd += 0x04;
+	readCmd += 0x51;
+	readCmd += 0x76;
+	readCmd += 0xee;
+	BOOST_CHECK(readCmd == zs.MakeReadCmd(ZSSerial::one, 21));
+}
+
 BOOST_AUTO_TEST_CASE(zsserial_readdata_test)
 {
-	ZSSerialProtocol protocol(zs_protocol_file);
 	ZSSerial zs("COM3", 9600, protocol);
 	std::vector<ZSDataItem> items = zs.ReadData(ZSSerial::one, 1);
 	BOOST_CHECK_EQUAL(items.size(), 12);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
