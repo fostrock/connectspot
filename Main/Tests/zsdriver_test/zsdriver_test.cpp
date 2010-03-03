3c6d9fa8c3d626de72b132bb2f2969e4e8d54dc9
huayunfly <huayunfly@gmail.com>
2010-03-03 09:14:01 +0000

    comment the example cases

--- Main/Tests/zsdriver_test/zsdriver_test.cpp
+++ Main/Tests/zsdriver_test/zsdriver_test.cpp
@@ -14,26 +14,26 @@
 #include <boost/test/unit_test.hpp>
 #include "opcspot/ZSSerial.h"
 
-class TestFixture {
-public:
-	TestFixture() : fixtureCount(0) { BOOST_TEST_MESSAGE("setup fixture"); }
-	~TestFixture() { BOOST_TEST_MESSAGE("teardown fixture"); }
-
-public:
-	int fixtureCount;
-};
-
-BOOST_FIXTURE_TEST_SUITE(suiteName, TestFixture)
-
-BOOST_AUTO_TEST_CASE(test_case1) {
-	BOOST_CHECK(fixtureCount == 0);
-}
-
-BOOST_AUTO_TEST_CASE(test_case2) {
-	BOOST_CHECK_EQUAL(fixtureCount, 0);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
+//class TestFixture {
+//public:
+//	TestFixture() : fixtureCount(0) { BOOST_TEST_MESSAGE("setup fixture"); }
+//	~TestFixture() { BOOST_TEST_MESSAGE("teardown fixture"); }
+//
+//public:
+//	int fixtureCount;
+//};
+//
+//BOOST_FIXTURE_TEST_SUITE(suiteName, TestFixture)
+//
+//BOOST_AUTO_TEST_CASE(test_case1) {
+//	BOOST_CHECK(fixtureCount == 0);
+//}
+//
+//BOOST_AUTO_TEST_CASE(test_case2) {
+//	BOOST_CHECK_EQUAL(fixtureCount, 0);
+//}
+//
+//BOOST_AUTO_TEST_SUITE_END()
 
 //// zsdriver_test.cpp : Defines the entry point for the console application.
 ////
