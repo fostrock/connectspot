f508182a33d15cf7e6358b50fb51f07128f7f3b1
huayunfly@gmail.com <huayunfly@gmail.com@c85c812c-0970-11df-bd4f-1fc3cb39d6f8>
2010-03-11 09:38:52 +0000

    integrate ZSSerial with ZSDriver

--- trunk/Main/Srcs/opcspot/ZSDriver.cpp
+++ trunk/Main/Srcs/opcspot/ZSDriver.cpp
@@ -10,13 +10,28 @@
 
 #include "StdAfx.h"
 #include "ZSDriver.h"
+#include "ZSSerial.h"
 
-ZSDriver::ZSDriver(void)
-{
-}
 
-ZSDriver::~ZSDriver(void)
+std::vector<boost::shared_ptr<ZSSerial> >* ZSDriver::serials = 0;
+
+bool ZSDriver::Init(const std::string& protocolPath)
 {
+	bool isOK = false;
+	serials = new std::vector<boost::shared_ptr<ZSSerial> >;
+	boost::shared_ptr<ZSSerialProtocol> protocol(new ZSSerialProtocol(protocolPath));
+	//if (!protocol->Parse())
+	//{
+	//	return isOK;
+	//}
+
+	//const std::vector<ZSSerialSetting>& ports = protocol->GetPortSetting();
+	//for (std::size_t i = 0; i < ports.size(); ++i)
+	//{
+	//	serials->push_back(boost::shared_ptr<ZSSerial>(new ZSSerial(ports.at(i).devName, *protocol)));
+	//}
+	
+	return false;
 }
 
 void ZSDriver::activation_monitor(const loCaller *ca, int count, loTagPair *til)
