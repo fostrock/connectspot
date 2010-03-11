c6d7e045d45819900dbf2e800a8c22dae655f719
huayunfly <huayunfly@gmail.com>
2010-03-11 09:37:42 +0000

    add .lib and include support

--- Main/Tests/opcspot_test/zsdriver_test.cpp
+++ Main/Tests/opcspot_test/zsdriver_test.cpp
@@ -1,23 +1,9 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include "opcspot/ZSDriver.h"
 
-class TestFixture {
-public:
-	TestFixture() : fixtureCount(0) { BOOST_TEST_MESSAGE("setup fixture"); }
-	~TestFixture() { BOOST_TEST_MESSAGE("teardown fixture"); }
 
-public:
-	int fixtureCount;
-};
-
-BOOST_FIXTURE_TEST_SUITE(suiteName, TestFixture)
-
-BOOST_AUTO_TEST_CASE(test_case1) {
-	BOOST_CHECK(fixtureCount == 1);
+BOOST_AUTO_TEST_CASE(zsdriver_init_test) 
+{
+	BOOST_CHECK_EQUAL(true, ZSDriver::Init("d:\\zsdriver.xml"));
 }
-
-BOOST_AUTO_TEST_CASE(test_case2) {
-	BOOST_CHECK_EQUAL(fixtureCount, 0);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
