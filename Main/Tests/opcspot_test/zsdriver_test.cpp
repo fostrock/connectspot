58dd5ef6302de837fd8368f0c54e997e9a5bc56c
huayunfly <huayunfly@gmail.com>
2010-03-18 15:56:52 +0000

    add test for ZSDriver::GetTagDef()

--- Main/Tests/opcspot_test/zsdriver_test.cpp
+++ Main/Tests/opcspot_test/zsdriver_test.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <boost/test/unit_test.hpp>
 #include "opcspot/ZSDriver.h"
+#include <iostream>
 
 
 BOOST_AUTO_TEST_CASE(zsdriver_methods_test) 
@@ -8,7 +9,11 @@ BOOST_AUTO_TEST_CASE(zsdriver_methods_test)
 	BOOST_CHECK_EQUAL(true, ZSDriver::Init("d:\\zsdriver.xml"));
 
 	std::vector<ZSDriver::TAG_DEF> tagDef = ZSDriver::GetTagDef();
-	BOOST_CHECK(tagDef.size() > 0);
+	BOOST_CHECK_EQUAL(tagDef.size(), 38 * 4);
+	for (std::size_t i = 0; i < tagDef.size(); i++)
+	{
+		ZSDriver::AssignTagIDIndexMap(tagDef.at(i).dataID + 1, tagDef.at(i).dataID);
+	}
 
 	ZSDriver::Destroy();
 }
