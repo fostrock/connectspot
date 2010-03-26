a602cffb43c24ba8310e05cc41112a0f5cbf7258
huayunfly <huayunfly@gmail.com>
2010-03-26 08:20:09 +0000

    add install package batch file

---
+++ Main/install_package.bat
@@ -0,0 +1,29 @@
+xcopy .\bin\opcspot.exe  .\install /s /y /i
+xcopy .\bin\opcspotPS.dll  .\install /s /y /i
+xcopy .\bin\config\zsdriver.xml  .\install\config /s /y /i
+
+xcopy .\bin\lo.dll  .\install /s /y /i
+xcopy .\bin\unilog.dll  .\install /s /y /i
+
+xcopy .\bin\boost_date_time-vc90-mt-1_41.dll  .\install /s /y /i
+xcopy .\bin\boost_system-vc90-mt-1_41.dll  .\install /s /y /i
+xcopy .\bin\boost_thread-vc90-mt-1_41.dll  .\install /s /y /i
+xcopy .\bin\glibmm-vc90-2_4.dll  .\install /s /y /i
+xcopy .\bin\iconv.dll  .\install /s /y /i
+xcopy .\bin\libglib-2.0-0.dll  .\install /s /y /i
+xcopy .\bin\libgmodule-2.0-0.dll  .\install /s /y /i
+xcopy .\bin\libgobject-2.0-0.dll  .\install /s /y /i
+xcopy .\bin\libxml2.dll  .\install /s /y /i
+xcopy .\bin\sigc-vc90-2_0.dll  .\install /s /y /i
+xcopy .\bin\xml++-vc90-2_6.dll  .\install /s /y /i
+xcopy .\bin\zlib1.dll  .\install /s /y /i
+
+xcopy .\bin\Microsoft.VC90.CRT.manifest  .\install /s /y /i
+xcopy .\bin\msvcr90.dll  .\install /s /y /i
+xcopy .\bin\msvcm90.dll  .\install /s /y /i
+xcopy .\bin\msvcp90.dll  .\install /s /y /i
+
+
+
+
+
