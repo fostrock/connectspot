22085c078979a51c242027057838e750ad8a4179
huayunfly <huayunfly@gmail.com>
2010-05-27 15:14:23 +0000

    fix the overlapped problem between the label and the text box.

--- Main/Srcs/opcspot_mgr/AddItemDlg.Designer.cs
+++ Main/Srcs/opcspot_mgr/AddItemDlg.Designer.cs
@@ -39,35 +39,36 @@
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(12, 37);
+            this.labelName.Location = new System.Drawing.Point(12, 34);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(29, 13);
+            this.labelName.Size = new System.Drawing.Size(35, 12);
             this.labelName.TabIndex = 0;
             this.labelName.Text = "label";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(53, 34);
+            this.textBox1.Location = new System.Drawing.Point(66, 31);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.Size = new System.Drawing.Size(174, 21);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // labelExp
             // 
             this.labelExp.AutoSize = true;
-            this.labelExp.Location = new System.Drawing.Point(12, 9);
+            this.labelExp.Location = new System.Drawing.Point(12, 8);
             this.labelExp.Name = "labelExp";
-            this.labelExp.Size = new System.Drawing.Size(146, 13);
+            this.labelExp.Size = new System.Drawing.Size(185, 12);
             this.labelExp.TabIndex = 2;
             this.labelExp.Text = "Item format can be: 1 or 1,2,5";
             // 
             // buttonOK
             // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOK.Enabled = false;
-            this.buttonOK.Location = new System.Drawing.Point(84, 90);
+            this.buttonOK.Location = new System.Drawing.Point(84, 83);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.Size = new System.Drawing.Size(75, 21);
             this.buttonOK.TabIndex = 3;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
@@ -75,10 +76,11 @@
             // 
             // buttonCancel
             // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(165, 90);
+            this.buttonCancel.Location = new System.Drawing.Point(165, 83);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(75, 21);
             this.buttonCancel.TabIndex = 4;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -88,19 +90,19 @@
             // 
             this.labelError.AutoSize = true;
             this.labelError.ForeColor = System.Drawing.Color.DarkRed;
-            this.labelError.Location = new System.Drawing.Point(50, 57);
+            this.labelError.Location = new System.Drawing.Point(50, 53);
             this.labelError.Name = "labelError";
-            this.labelError.Size = new System.Drawing.Size(22, 13);
+            this.labelError.Size = new System.Drawing.Size(35, 12);
             this.labelError.TabIndex = 5;
             this.labelError.Text = "     ";
             // 
             // AddItemDlg
             // 
             this.AcceptButton = this.buttonOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(252, 125);
+            this.ClientSize = new System.Drawing.Size(252, 115);
             this.ControlBox = false;
             this.Controls.Add(this.labelError);
             this.Controls.Add(this.buttonCancel);
